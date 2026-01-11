0450ac59d0e1ee09861de8c46c3155b66f0b554b
vgraman0 <vgraman0@gmail.com>
2026-01-11 12:45:04 +0000

    Deploying to gh-pages from @ vgraman0/vgraman0.github.io@5992435c36ed6f1a778e823b4b57b03e604fd54e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -34,7 +34,7 @@ ninja.data = [{
         
           title: "Chapter 3: Hedging Strategies Using Futures",
         
-        description: "From Hull, *Options, Futures, and Other Derivatives*",
+        description: "From Hull, Options, Futures, and Other Derivatives",
         section: "Posts",
         handler: () => {
           
