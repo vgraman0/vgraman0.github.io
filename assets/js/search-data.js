b14806b9abab5378e02a8c3c5db7cbc4b0b44bb0
vgraman0 <vgraman0@gmail.com>
2026-01-15 14:04:37 +0000

    Deploying to gh-pages from @ vgraman0/vgraman0.github.io@4e2ef94560578f4b47be146b0e125146643c708f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,6 +41,17 @@ ninja.data = [{
             window.location.href = "/blog/2026/ddia-ch1/";
           
         },
+      },{id: "post-hull-ch-4-interest-rates",
+        
+          title: "Hull Ch 4: Interest Rates",
+        
+        description: "From Hull, Options, Futures, and Other Derivatives",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/hull-options-ch4/";
+          
+        },
       },{id: "post-hull-ch-3-hedging-strategies-using-futures",
         
           title: "Hull Ch 3: Hedging Strategies Using Futures",
